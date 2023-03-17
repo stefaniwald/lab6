7eee1824bc3d303b1dac2576e69d067dc4b00a03
stefaniwald <stefaniwald@ufl.edu>
2023-03-17 12:18:04 -0400

    add encoder

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def encode(password):
         new_pass += str(int(num) + 3)
     return new_pass
 
-
-while True:
-    menu()
-    menu_op = int(input('\nPlease enter an option: '))
-    if menu_op == 1:
-        original_password = input('Please enter your password to encode ')
-        encoded_password = encode(original_password)
-        print('Your password has been encoded and stored!\n')
-    elif menu_op == 2:
-        print(f'The encoded password is {encoded_password}, and the original password is {original_password}.\n')
-    else:
-        break
+if __name__ == "__main__":
+    while True:
+        menu()
+        menu_op = int(input('\nPlease enter an option: '))
+        if menu_op == 1:
+            original_password = input('Please enter your password to encode ')
+            encoded_password = encode(original_password)
+            print('Your password has been encoded and stored!\n')
+        elif menu_op == 2:
+            print(f'The encoded password is {encoded_password}, and the original password is {original_password}.\n')
+        else:
+            break
