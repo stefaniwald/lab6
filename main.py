ccb060168f563065da336ae26b9bdd07f95e36a5
stefaniwald <stefaniwald@ufl.edu>
2023-03-19 11:08:22 -0400

    added my name

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def menu():
     print('2. Decode ')
     print('3. Quit')
 
+
 def encode(password):       # encodes password by shifting each number up by 3
     new_pass = ''
     for num in password:
@@ -13,8 +14,8 @@ def encode(password):       # encodes password by shifting each number up by 3
             new_pass += str(int(num) + 3)
         else:
             new_pass += str(int(num) - 7)
-
     return new_pass
+
 
 if __name__ == "__main__":
     while True:
